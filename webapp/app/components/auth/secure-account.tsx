0afddad892d5d3e86ac987356be9a4eef82225b7
Saloni Agrawal <saloniagrawal250797@gmail.com>
2024-12-16 21:50:28 +0530

    User Story 826: Multi Factor Authentication

--- webapp/app/components/auth/secure-account.tsx
+++ webapp/app/components/auth/secure-account.tsx
@@ -3,6 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogHeader } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
+import { DictionaryType } from "@/types";
 import { useFormik } from "formik";
 import { ChevronRight, Loader2Icon, Phone } from "lucide-react";
 import dynamic from "next/dynamic";
@@ -20,15 +21,16 @@ const LoadingWithBackground = dynamic(
 );
 
 interface Props {
-  setOpen: Dispatch<SetStateAction<boolean>>;
-  open: boolean;
+  readonly setOpen: Dispatch<SetStateAction<boolean>>;
+  readonly open: boolean;
+  readonly dictionary: DictionaryType;
 }
 
 const validationSchema = yup.object({
   phone: yup.string().required("Phone is required"),
 });
 
-export default function SecureAccount({ setOpen, open }: Props) {
+export default function SecureAccount({ setOpen, open, dictionary }: Props) {
   const [contactType, setContactType] = useState(1);
   const [openLoading, setOpenLoading] = useState(false);
   const [showCode, setShowCode] = useState(false);
@@ -57,38 +59,42 @@ export default function SecureAccount({ setOpen, open }: Props) {
   return (
     <>
       <Dialog open={open} onOpenChange={setOpen}>
-        <DialogContent className="max-w-[580px]">
+        <DialogContent className="max-w-[580px] p-[2.5rem]">
           <DialogHeader>
-            <h1 className="text-3xl font-poppins font-bold text-center text-gray-dark">
-              Secure Your Account
+            <h1 className="text-3xl font-poppins font-bold text-center text-gray-dark tracking-[1px]">
+              {dictionary.auth.secure_your_account}
             </h1>
           </DialogHeader>
           <div className=" text-center px-4">
-            <p className="text-lg text-gray-dark mt-10 font-bold ">
+            <p className="text-lg text-gray-dark mt-6 font-bold tracking-[0.5px]">
               {showCode
-                ? "Confirm Your Phone Number"
-                : "Receive authentication via phone"}
+                ? dictionary.auth.confirm_your_phone_number
+                : dictionary.auth.receive_authentication_via_phone}
             </p>
-            <p className="text-base text-[#404041] mt-4">
+            <p className="text-base text-[#404041] mt-4 tracking-[0.5px]">
               {showCode
-                ? `Please enter the 6 digits code  sent to ${phone}`
-                : " Be sure to use a phone number you have access to whenever you plan to sign in"}
+                ? `${
+                    dictionary.auth.please_enter_the_6_digits_code_sent_to
+                  } ${phone.replace(/^(\d{3})(\d{3})/, "XXX-XXX-")}`
+                : dictionary.auth.use_accessible_phone_number}
             </p>
             {showCode ? (
               <>
                 <VerificationInput
                   classNames={{
                     character:
-                      "border-[#D6CCF1] rounded-[5px]  w-[70px] h-[70px] flex items-center justify-center",
-                    container: "w-full text-center my-8",
+                      "border-[#D6CCF1] rounded-[5px] w-[70px] h-[70px] flex items-center justify-center",
+                    container: "w-full text-center my-12",
                     characterInactive: "bg-white",
+                    characterFilled:
+                      "text-[2rem] leading-[3rem] tracking-[1px] font-bold font-poppins",
                   }}
                   placeholder=""
                   value={code}
                   onChange={(e) => setCode(e)}
                 />
-                <p className="mt-6 text-gray-dark">
-                  Text or data rates may apply.
+                <p className="mt-8 text-gray-dark text-lg tracking-[0.5px] font-bold">
+                  {dictionary.auth.text_or_data_rates_may_apply}
                 </p>
                 <Button
                   disabled={!code || code.length !== 6}
@@ -98,20 +104,20 @@ export default function SecureAccount({ setOpen, open }: Props) {
                     setShowCode(false);
                     setOpen(false);
                   }}
-                  className="mt-6"
+                  className="mt-6 uppercase font-bold text-base tracking-[0.5px] disabled:bg-[#D6CCF1] disabled:opacity-100"
                 >
-                  NEXT <ChevronRight />
+                  {dictionary.auth.continue} <ChevronRight />
                 </Button>
               </>
             ) : (
               <>
                 <div className="grid grid-cols-2 gap-6 my-8">
-                  <div
+                  <button
                     className={`border ${
                       contactType === 1
                         ? "border-primary bg-[#F0ECFA]"
                         : "border-primary-foreground"
-                    } flex flex-col justify-center items-center p-4 rounded-md text-gray-dark cursor-pointer`}
+                    } flex flex-col justify-center items-center p-4 rounded-lg text-gray-dark cursor-pointer`}
                     onClick={() => setContactType(1)}
                   >
                     <Image
@@ -121,14 +127,16 @@ export default function SecureAccount({ setOpen, open }: Props) {
                       width={80}
                       height={80}
                     />
-                    Text Me
-                  </div>
-                  <div
+                    <p className="text-lg tracking-[0.5px] font-bold">
+                      {dictionary.auth.text_me}
+                    </p>
+                  </button>
+                  <button
                     className={`border ${
                       contactType === 2
                         ? "border-primary bg-[#F0ECFA]"
                         : "border-primary-foreground"
-                    } flex flex-col justify-center items-center p-4 rounded-md text-gray-dark cursor-pointer`}
+                    } flex flex-col justify-center items-center p-4 rounded-lg text-gray-dark cursor-pointer`}
                     onClick={() => setContactType(2)}
                   >
                     <Image
@@ -138,8 +146,10 @@ export default function SecureAccount({ setOpen, open }: Props) {
                       height={82}
                       className="mb-2"
                     />
-                    Call Me
-                  </div>
+                    <p className="text-lg tracking-[0.5px] font-bold">
+                      {dictionary.auth.call_me}
+                    </p>
+                  </button>
                 </div>
                 <form onSubmit={formik.handleSubmit} className="mt-8">
                   <Input
@@ -153,11 +163,14 @@ export default function SecureAccount({ setOpen, open }: Props) {
                     onChange={formik.handleChange}
                     icon={<Phone className="text-gray-mid" size={18} />}
                   />
-                  <p className="mt-6 text-gray-dark font-bold">
-                    Text or data rates may apply.
+                  <p className="mt-6 text-lg tracking-[0.5px] text-gray-dark font-bold">
+                    {dictionary.auth.text_or_data_rates_may_apply}
                   </p>
-                  <Button type="submit" className="mt-6">
-                    Continue <ChevronRight />
+                  <Button
+                    type="submit"
+                    className="mt-6 uppercase font-bold text-base tracking-[0.5px]"
+                  >
+                    {dictionary.auth.continue} <ChevronRight />
                   </Button>
                 </form>
               </>
@@ -166,18 +179,18 @@ export default function SecureAccount({ setOpen, open }: Props) {
             <Button
               variant={"outline"}
               type="button"
-              className="mt-4"
+              className="mt-4 uppercase font-bold text-base tracking-[0.5px]"
               onClick={handleCloseModal}
             >
-              Go Back
+              {dictionary.auth.go_back}
             </Button>
           </div>
         </DialogContent>
       </Dialog>
       {openLoading && (
         <LoadingWithBackground
-          title="Thanks for verifying it’s you!"
-          subTitle="One moment, we’re logging you in..."
+          title={dictionary.auth.thanks_for_verifying_its_you}
+          subTitle={dictionary.auth.logging_you_in}
           open={openLoading}
           setOpen={setOpenLoading}
         />
